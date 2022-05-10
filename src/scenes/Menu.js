8db0bd75088d3ee6896d1dc82b28d0100b05bd5d
“Jane <ttran161@ucsc.edu>
2022-05-10 11:43:53 -0700

    font

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -9,6 +9,19 @@ class Menu extends Phaser.Scene {
 
     create() {
         // more stuff
+        let menuConfig = {
+            fontFamily: 'Horror',
+            fontSize: '50px',
+            color: '#eeeeee',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        this.add.text(game.config.width/2, game.config.height/2, 'Put on Your Pretty Face', menuConfig).setOrigin(0.5, 2.5);
+
         this.scene.start("filterGame");
     }
 
