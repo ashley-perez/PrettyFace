6c0c08f7043cd13060cdaeae6e7915c99be28583
ashley-perez <ashleyperez428@gmail.com>
2022-05-13 15:40:28 -0700

    character animation and some eye stuff

--- src/scenes/Eyes.js
+++ src/scenes/Eyes.js
@@ -5,19 +5,23 @@ class Eyes extends Phaser.Scene {
 
     preload() {
         this.load.image('pupil', './assets/pupil.png');
+
+        this.load.image('eye', './assets/placeHolderEye.png');
     }
 
     create() {
-        var eyeLeft = this.add.ellipse(480, 110, 230, 90, 0xFFFFFF);
-        var eyeRight = this.add.ellipse(790, 110, 230, 90, 0xFFFFFF);
-        var pupilLeft = this.add.sprite(480,110, 'pupil');
-        var pupilRight = this.physics.add.image(790, 110, 'pupil');
+        // var eyeLeft = this.add.ellipse(480, 110, 230, 90, 0xFFFFFF);
+        // var eyeRight = this.add.ellipse(790, 110, 230, 90, 0xFFFFFF);
+        // var pupilLeft = this.add.sprite(480,110, 'pupil');
+        // var pupilRight = this.physics.add.image(790, 110, 'pupil');
+
+        this.eye = this.physics.add.sprite(480, 110, 'eye');
 
         // pupil follow to mouse object
-        this.input.on('pointermove', function (pointer)
-    {
-        this.physics.moveToObject(pupilRight, pointer, 200);
-    }, this);
+        this.input.on('pointermove', function (pointer) {
+            this.physics.moveToObject(this.eye, pointer, 200);
+        }, this);
+
     }
 
 }
--- src/scenes/Narrative_1.js
+++ src/scenes/Narrative_1.js
@@ -6,15 +6,13 @@ class Cutscene extends Phaser.Scene {
     }
 
     preload() {
-        // stuff
-        this.load.atlas('link_atlas', './assets/linksheet.png', './assets/link.json');
+        // loading player texture atlas
+        this.load.atlas('girl_atlas', './assets/testAtlas.png', './assets/testWalk.json');
     }
 
     create() {
-        this.testBox = this.add.text(100, 100, '', {color: '#FFFFFF'}).setWordWrapWidth(500); // empty '' needs to be there!!
-	    this.typewriteText('really good writing goes here', this.testBox, 70);
-        // array of dialogue
-        this.wordArray = ['really good writing goes here', 'hello', 'bye'];
+        // array of dialogue 
+        this.wordArray = ['really good writing goes here', 'hello', 'bruh', 'bye'];
         this.index = 0;
 
         // text boxes that "write themselves"
@@ -22,7 +20,7 @@ class Cutscene extends Phaser.Scene {
 	    this.typewriteText(this.wordArray[this.index], this.testBox, 70);
 
         // player stuff
-        this.player = this.physics.add.sprite(game.config.width-200, game.config.height/2, 'link_atlas', 'idle_down_001').setScale(0.5);
+        this.player = this.physics.add.sprite(game.config.width-200, game.config.height/2, 'girl_atlas', 'walk_left_0001').setScale(0.5);
         this.charMoveSpeed = 2.5;
 
         // key inputs
@@ -33,139 +31,58 @@ class Cutscene extends Phaser.Scene {
 
         // player animations
         this.anims.create({
-            key: 'idle_left',
-            frames: this.anims.generateFrameNames('link_atlas', {
-                prefix: 'idle_left_',
+            key: 'walk_left',
+            frames: this.anims.generateFrameNames('girl_atlas', {
+                prefix: 'walk_left_',
                 start: 1,
-                end: 3,
+                end: 4,
                 suffix: '',
                 zeroPad: 4
             }),
             frameRate: 15,
             repeat: -1,
-            repeatDelay: 3000,
-            yoyo: true
         });
 
         this.anims.create({
-            key: 'idle_right',
-            frames: this.anims.generateFrameNames('link_atlas', {
-                prefix: 'idle_right_',
+            key: 'walk_right',
+            frames: this.anims.generateFrameNames('girl_atlas', {
+                prefix: 'walk_right_',
                 start: 1,
-                end: 3,
+                end: 4,
                 suffix: '',
                 zeroPad: 4
             }),
             frameRate: 15,
             repeat: -1,
-            repeatDelay: 3000,
-            yoyo: true
         });
 
-        this.anims.create({
-            key: 'idle_up',
-            frames: this.anims.generateFrameNames('link_atlas', {
-                prefix: 'idle_up_',
-                start: 1,
-                end: 1,
-                zeroPad: 4
-            }),
-            frameRate: 0,
-            repeat: -1,
-        });
-
-        this.anims.create({
-            key: 'idle_down',
-            frames: this.anims.generateFrameNames('link_atlas', {
-                prefix: 'idle_down_',
-                start: 1,
-                end: 3,
-                suffix: '',
-                zeroPad: 4
-            }),
-            frameRate: 15,
-            repeat: -1,
-            repeatDelay: 3000,
-            yoyo: true
-        });
-
-        this.anims.create({
-            key: 'run_left',
-            frames: this.anims.generateFrameNames('link_atlas', {
-                prefix: 'run_left_',
-                start: 1,
-                end: 10,
-                suffix: '',
-                zeroPad: 4
-            }),
-            frameRate: 30,
-            repeat: -1,
-        });
-
-        this.anims.create({
-            key: 'run_right',
-            frames: this.anims.generateFrameNames('link_atlas', {
-                prefix: 'run_right_',
-                start: 1,
-                end: 10,
-                suffix: '',
-                zeroPad: 4
-            }),
-            frameRate: 30,
-            repeat: -1,
-        });
-
-        this.anims.create({
-            key: 'run_up',
-            frames: this.anims.generateFrameNames('link_atlas', {
-                prefix: 'run_up_',
-                start: 1,
-                end: 10,
-                suffix: '',
-                zeroPad: 4
-            }),
-            frameRate: 30,
-            repeat: -1,
-        });
-
-        this.anims.create({
-            key: 'run_down',
-            frames: this.anims.generateFrameNames('link_atlas', {
-                prefix: 'run_down_',
-                start: 1,
-                end: 10,
-                suffix: '',
-                zeroPad: 4
-            }),
-            frameRate: 30,
-            repeat: -1,
-        });
-
-        this.time = 0;
         this.complete = true;
-
+        this.timer = 0;
 
     }
 
     update() {
+
         // switch scene to eyes
         if(this.complete == true) {
-            this.time += 0.01;
+            this.timer += 0.01;
         }
-        if (this.time >= 5) {
+        if (this.timer >= 7) {
             this.scene.start("eyesGame");
         }
+        
         // click for more text to be written
         if (this.input.activePointer.isDown && textDone == true) {
-            this.index += 1;
+            this.index += 1; 
 
+            // so out of index error doesn't happen (clicked through all the dialogue)
             if (this.index >= this.wordArray.length) {
                 textDone = false;
                 return;
             }
 
-            this.testBox.text = '';
-            this.typewriteText(this.wordArray[this.index], this.testBox, 70);
+            this.testBox.text = ''; // reset the text
+            this.typewriteText(this.wordArray[this.index], this.testBox, 60); 
             console.log('SLAYYYYYY');
             textDone = false;
 
@@ -175,51 +92,52 @@ class Cutscene extends Phaser.Scene {
         // thank you Prof for making the texture atlas lab
         if (keyRIGHT.isDown) {
             this.player.x += this.charMoveSpeed;
-            this.player.anims.play('run_right', true);
+            this.player.anims.play('walk_right', true);
         }
         else if (keyLEFT.isDown) {
             this.player.x -= this.charMoveSpeed;
-            this.player.anims.play('run_left', true);
+            this.player.anims.play('walk_left', true);
         }
-        else if (keyUP.isDown) {
-            this.player.y -= this.charMoveSpeed;
-            this.player.anims.play('run_up', true);
-        }
-        else if (keyDOWN.isDown) {
-            this.player.y += this.charMoveSpeed;
-            this.player.anims.play('run_down', true);
-        }
+        // else if (keyUP.isDown) {
+        //     this.player.y -= this.charMoveSpeed;
+        //     this.player.anims.play('run_up', true);
+        // }
+        // else if (keyDOWN.isDown) {
+        //     this.player.y += this.charMoveSpeed;
+        //     this.player.anims.play('run_down', true);
+        // }
         // idle animations
-        else if (!keyRIGHT.isDown && !keyLEFT.isDown && !keyUP.isDown && !keyDOWN.isDown) {
+        // else if (!keyRIGHT.isDown && !keyLEFT.isDown && !keyUP.isDown && !keyDOWN.isDown) {
+            
+        //     if (this.player.anims.isPlaying && this.player.anims.currentAnim.key === 'run_left') {
+        //         this.player.anims.play('idle_left');
+        //     }
+        //     if (this.player.anims.isPlaying && this.player.anims.currentAnim.key === 'run_right') {
+        //         this.player.anims.play('idle_right');
+        //     }
+        //     if (this.player.anims.isPlaying && this.player.anims.currentAnim.key === 'run_up') {
+        //         this.player.anims.play('idle_up');
+        //     }
+        //     if (this.player.anims.isPlaying && this.player.anims.currentAnim.key === 'run_down') {
+        //         this.player.anims.play('idle_down');
+        //     }
 
-            if (this.player.anims.isPlaying && this.player.anims.currentAnim.key === 'run_left') {
-                this.player.anims.play('idle_left');
-            }
-            if (this.player.anims.isPlaying && this.player.anims.currentAnim.key === 'run_right') {
-                this.player.anims.play('idle_right');
-            }
-            if (this.player.anims.isPlaying && this.player.anims.currentAnim.key === 'run_up') {
-                this.player.anims.play('idle_up');
-            }
-            if (this.player.anims.isPlaying && this.player.anims.currentAnim.key === 'run_down') {
-                this.player.anims.play('idle_down');
-            }
-
-        }
+        // }
     }
-
-    // credit for this function: https://tinyurl.com/typewritephaser3
+    
+    // credit for the base of this function: https://tinyurl.com/typewritephaser3
     // allows text to slowly be written like in other rpg games
+    // has been modified to better suit our needs :)
     typewriteText(text, textbox, speed) {
-        const length = text.length; // how many times the loop should repeat (based on string length)
+        const length = text.length; // how many times the loop should repeat (based on sentence length)
         let i = 0;
-        textDone = false;
+        //textDone = false;
         this.time.addEvent({
             callback: () => {
                 textbox.text += text[i]
                 ++i
-                console.log('hi: ', i);
 
+                // when there is no more to write set bool to false
                 if (i == length) {
                     textDone = true;
                 }
@@ -227,5 +145,5 @@ class Cutscene extends Phaser.Scene {
             repeat: length - 1,
             delay: speed // typing speed, big numbers = slower text, small = faster
         });
-        }
     }
+}
