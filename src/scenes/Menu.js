d117e26424a35ee29fa803c3716e2845871a5cb8
“Jane <ttran161@ucsc.edu>
2022-05-24 18:23:51 -0700

    menu background

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -7,19 +7,42 @@ class Menu extends Phaser.Scene {
         //this.load.spritesheet('button', './assets/playButton.png');
         //this.load.image('mouse', './assets/mouse.png');
         this.load.audio('bgMusic', './assets/bg_music.mp3');
+        this.load.audio('start', './assets/kiss.wav');
         this.load.image('play1', './assets/play1.png');
         this.load.image('play2', './assets/play2.png');
+        this.load.image('bg', './assets/bg_menu.png');
     }
 
     create() {
         // gamePhase = 1;
-
         health = 100;
-        // more stuff
-        let menuConfig = {
+
+        let title1Config = {
             fontFamily: 'Horror',
-            fontSize: '80px',
-            color: '#FFFFFF',
+            fontSize: '78px',
+            color: '#ba1141',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        let title2Config = {
+            fontFamily: 'Horror',
+            fontSize: '109px',
+            color: '#ba1141',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        let startConfig = {
+            fontFamily: 'Monster',
+            fontSize: '40px',
+            color: '#ff99b9',
             align: 'right',
             padding: {
                 top: 5,
@@ -28,26 +51,18 @@ class Menu extends Phaser.Scene {
             fixedWidth: 0
         }
 
-        let keyConfig = {
-            fontFamily: 'Horror',
-            fontSize: '35px',
-            color: '#FFFFFF',
-            align: 'right',
-            padding: {
-                top: 5,
-                bottom: 5,
-            },
-            fixedWidth: 0
-        }
-
-        this.add.text(game.config.width/2, game.config.height/2, 'Put on Your Pretty Face', menuConfig).setOrigin(0.5, 2.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'Press S to Start', keyConfig).setOrigin(0.5,0.7);
+        this.add.image(0,0, "bg").setOrigin(0).setDepth(0);
+        this.add.text(game.config.width/2, game.config.height/2, 'Put on your', title1Config).setOrigin(0.5, 2.1);
+        this.add.text(game.config.width/2, game.config.height/2, 'Pretty Face', title2Config).setOrigin(0.5, -0.45);
+        this.add.rectangle(644, 649, 395, 85, 0x5c4ae4);
+        this.add.text(game.config.width/2, game.config.height/2, 'Press S to Start', startConfig).setOrigin(0.5,-5);
 
         keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
     }
 
     update() {
         if (Phaser.Input.Keyboard.JustDown(keyS)) {
+            this.sound.play('start');
             var music = this.sound.add('bgMusic', {volume: 0.2});  // add music background
             music.setLoop(true);
             music.play();
