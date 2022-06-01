9d7ffa02ee6243409971b313ed1a293a106f50b6
Jane Tran <tranjtrang@gmail.com>
2022-05-31 23:34:25 -0700

    flashy final scene

--- src/scenes/Broken.js
+++ src/scenes/Broken.js
@@ -4,7 +4,7 @@ class Broken extends Phaser.Scene {
     }
 
     preload() {
-
+        this.load.atlas('brokenBg','./assets/finalsheet.png','./assets/finalScene.json');
     }
 
     create() {
@@ -21,6 +21,21 @@ class Broken extends Phaser.Scene {
         }
 
         this.textBox = this.add.text(200, 200, 'hello', normalWriting);
+
+        this.anims.create({
+            key: 'broken',
+            frames: this.anims.generateFrameNames('brokenBg', {
+                prefix: 'sprite',
+                start: 1,
+                end: 3,
+                suffix: '',
+                zeroPad: 1
+            }),
+            frameRate: 15,
+            repeat: -1,
+        });
+        this.finalScene = this.physics.add.sprite(config.width/2, config.height/2, 'brokenBg', 0);
+        this.finalScene.anims.play('broken', true);
     }
 
     update() {
